e3563f15073882d53bb448d702dce6277e4454c0
Xqst <gatdula.raymondcp@gmail.com>
2022-05-26 19:44:49 +0800

    experimented with PriorityQueue
    Don't know how to arrange from least to greatest the sorting of PQ but I saw that I need to implement either heap sort. Will try next time

--- Advance-DataStructs/src/Main_Queue.java
+++ Advance-DataStructs/src/Main_Queue.java
@@ -2,22 +2,36 @@ import java.util.*;
 public class Main_Queue {
 
     public static void main(String[] args) {
-        Queue queue1 = new LinkedList(); //can use Queue<Integer> queue1 = new LinkedList<Integer>(); to be more specific on what data type
+        Queue queue1 = new LinkedList();//can use Queue<Integer> queue1 = new LinkedList<Integer>(); to be more specific on what data type
+        PriorityQueue pq1 = new PriorityQueue();
         Scanner in = new Scanner(System.in);
-        int size;
+        Scanner pq = new Scanner(System.in);
+        String yesno;
+        int size, pqSize = 0;
         System.out.println("Welcome to the program! ");
         System.out.print("How long is the queue: ");
         size = in.nextInt();
         int [] arrayInt = new int[size];
+
+        System.out.print("Are there any people with disability or senior citizens: ");
+        yesno = pq.nextLine();
+        if (yesno.equalsIgnoreCase("yes")){
+            System.out.print("How many are there: ");
+            pqSize = in.nextInt();
+            int [] arrayPQ = new int[pqSize];
+            System.out.println("Please enter their ticket Nos.: ");
+            for (int i = 0; i<pqSize; i++){
+                arrayPQ[i] = in.nextInt();
+                pq1.add(arrayPQ[i]);
+            }
+        }
+
         System.out.print("Please enter the numbers in queue: ");
-        for (int i = 0; i<size; i++){
+        for (int i = 0; i<size-pqSize; i++){
             arrayInt[i] = in.nextInt();
             queue1.add(arrayInt[i]);
         }
 
-        System.out.println("Current queue: " + queue1);
-        System.out.println("Removed element: " + queue1.remove());
-        System.out.println("New head of the queue: " + queue1.peek());
-        System.out.println("Final queue: " + queue1);
+        System.out.println("The final queue is: " + pq1 + queue1);
     }
 }
